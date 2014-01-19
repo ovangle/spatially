734873cef6a41828bf85d522c218f8314f09a8d6
Thomas Stephenson <ovangle@gmail.com>
2014-01-19 22:45:22 +1100

    Coordinte locator tests
    Coordinate locator tests are now run as part of algorithm/all_tests.dart

--- test/algorithm/all_tests.dart
+++ test/algorithm/all_tests.dart
@@ -1,10 +1,12 @@
 library spatially.algorithm.all_tests;
 
 import 'package:unittest/unittest.dart';
+import 'cg_algorithm_test.dart' as cg_algorithm;
 import 'line_intersector_tests.dart' as line_intersector;
 
 main() {
   group("algorithm: ", () {
+    cg_algorithm.main();
     line_intersector.main();
 
   });
