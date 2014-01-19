79e0faa38fc2ef76251b80b0573a7ea1df440f89
Thomas Stephenson <ovangle@gmail.com>
2014-01-20 07:10:51 +1100

    Added coordinate locator to algorithm.all_tests

--- test/algorithm/all_tests.dart
+++ test/algorithm/all_tests.dart
@@ -3,11 +3,13 @@ library spatially.algorithm.all_tests;
 import 'package:unittest/unittest.dart';
 import 'cg_algorithm_test.dart' as cg_algorithm;
 import 'line_intersector_tests.dart' as line_intersector;
+import 'coordinate_locator_test.dart' as coordinate_locator;
 
 main() {
   group("algorithm: ", () {
     cg_algorithm.main();
     line_intersector.main();
+    coordinate_locator.main();
 
   });
 }
