9d12e2ab258da845af86b0d4b6a04c1ece96df47
Thomas Stephenson <ovangle@gmail.com>
2014-01-22 22:39:33 +1100

    Fixed problem when comparing points to empty geometries

--- lib/geom/src/base/point.dart
+++ lib/geom/src/base/point.dart
@@ -5,29 +5,29 @@ class Point extends Geometry {
    * The [Coordinate] wrapped by this [Point]
    */
   CoordinateSequence _coords;
-  Point._(CoordinateSequence this._coords, GeometryFactory factory) 
+  Point._(CoordinateSequence this._coords, GeometryFactory factory)
       : super._(factory);
-  
-  Coordinate get coordinate => 
+
+  Coordinate get coordinate =>
       isNotEmptyGeometry ? _coords.first : null;
-  
+
   bool get isEmptyGeometry => _coords.length == 0;
-  
+
   bool get isSimple => true;
-  
+
   int get dimension => dim.POINT;
-  
+
   int get boundaryDimension => dim.EMPTY;
-  
+
   Geometry get copy => factory.createPoint(new Coordinate.copy(coordinate));
-  
+
   Geometry get reversed => copy;
-  
-  Geometry get boundary => 
+
+  Geometry get boundary =>
       factory.createEmptyGeometryList();
-  
+
   Array<Coordinate> get coordinates => _coords.toArray();
-  
+
   Envelope _computeEnvelope() {
     Envelope env = new Envelope.empty();
     if (isEmptyGeometry) {
@@ -35,10 +35,11 @@ class Point extends Geometry {
     }
     return env.expandToCoordinate(this.coordinate);
   }
-  
+
   bool equalsExact(Geometry other, [double tolerance = 0.0]) {
     if (other is Point) {
       if (isEmptyGeometry && other.isEmptyGeometry) return true;
+      if (isEmptyGeometry || other.isEmptyGeometry) return false;
       if (tolerance > 0) {
         return coordinate == other.coordinate;
       }
@@ -46,15 +47,15 @@ class Point extends Geometry {
     }
     return false;
   }
-  
-  
+
+
   void normalize() {
     // a point is always normalized
   }
-  
+
   int _compareToSameType(Point other, Comparator<CoordinateSequence> comparator) {
     return comparator(_coords, other._coords);
   }
-  
-  
+
+
 }
