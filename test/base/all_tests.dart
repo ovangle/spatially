88207c84e6b49679da5cbe2dca328c8c98291465
Thomas Stephenson <ovangle@gmail.com>
2014-01-24 22:36:41 +1100

    Added generators to dart

--- test/base/all_tests.dart
+++ test/base/all_tests.dart
@@ -5,6 +5,8 @@ import 'package:unittest/unittest.dart';
 import 'iterables/all_tests.dart' as iterables;
 import 'base_tests.dart' as base;
 import 'coordinate_test.dart' as coordinate;
+import 'error_test.dart' as error;
+import 'generator_test.dart' as generator;
 import 'graph_tests.dart' as graph;
 import 'line_segment_test.dart' as line_segment;
 import 'linkedlist_test.dart' as linkedlist;
@@ -14,6 +16,8 @@ void main() {
   group("base: ", () {
     base.main();
     coordinate.main();
+    generator.main();
+    error.main();
     graph.main();
     line_segment.main();
     linkedlist.main();
