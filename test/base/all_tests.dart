5f2fc346480d72d0a91ff8bc60d2e1069932b9d3
Thomas Stephenson <ovangle@gmail.com>
2014-01-26 12:04:46 +1100

    Removed generator from spatially.base
    After a chat with lasse on dartmiscc, decided it wasn't a good idea for spatially

--- test/base/all_tests.dart
+++ test/base/all_tests.dart
@@ -5,8 +5,6 @@ import 'package:unittest/unittest.dart';
 import 'iterables/all_tests.dart' as iterables;
 import 'base_tests.dart' as base;
 import 'coordinate_test.dart' as coordinate;
-import 'error_test.dart' as error;
-import 'generator_test.dart' as generator;
 import 'graph_tests.dart' as graph;
 import 'line_segment_test.dart' as line_segment;
 import 'linkedlist_test.dart' as linkedlist;
@@ -16,8 +14,6 @@ void main() {
   group("base: ", () {
     base.main();
     coordinate.main();
-    generator.main();
-    error.main();
     graph.main();
     line_segment.main();
     linkedlist.main();
